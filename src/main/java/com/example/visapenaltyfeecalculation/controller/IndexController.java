400d2e4da81906dd2d0e4a4ad2a2901726fcb6e7
Allahshukur <agazade.424@gmail.com>
2022-03-27 00:37:03 +0400

    new file add

--- src/main/java/com/example/visapenaltyfeecalculation/controller/IndexController.java
+++ src/main/java/com/example/visapenaltyfeecalculation/controller/IndexController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-    @RequestMapping("/calculator")
+    @RequestMapping("/index")
     public String calculator(){
-        return "calculator";
+        return "index";
     }
 
 }
